f77f5093546645d4ddfb0c7eaf7a7f1bb7356af2
Jan Helge Wolf <janhelgewolf@yahoo.de>
2015-03-08 12:57:04 +0100

    Initial Exporter commit (stub)

---
+++ src/exporters/Exporter.java
@@ -0,0 +1,16 @@
+package exporters;
+
+import java.util.Set;
+
+import articles.Article;
+
+public class Exporter {
+
+	public Exporter(Set<Article> articles) {
+
+	}
+
+	public void readArticles(Set<Article> articles) {
+
+	}
+}
